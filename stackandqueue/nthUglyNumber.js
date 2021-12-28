5ecf3e2a6764a23df44b50f9183b6b7a97df350e
Theseus <605572714@qq.com>
2021-12-28 18:10:09 +0800

    移除石子的最大得分 丑数 II
    移除石子的最大得分 丑数 II

--- stackandqueue/nthUglyNumber.js
+++ stackandqueue/nthUglyNumber.js
@@ -1,3 +1,47 @@
+// 264. 丑数 II
+
+// 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+
+// 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+
+
+
+// 示例 1：
+
+// 输入：n = 10
+// 输出：12
+// 解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+
+// 示例 2：
+
+// 输入：n = 1
+// 输出：1
+// 解释：1 通常被视为丑数。
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function (n) {
+  const factors = [2, 3, 5];
+  const seen = new Set();
+  const heap = new Heap("small");
+  seen.add(1);
+  heap.push(1);
+  let ugly = 0;
+  for (let i = 1; i < n; i++) {
+    ugly = heap.pop();
+    for (const factor of factors) {
+      const next = ugly * factor;
+      if (!seen.has(next)) {
+        seen.add(next);
+        heap.push(next);
+      }
+    }
+  }
+
+  return ugly;
+};
+
 class Heap {
   constructor(cmp = "large") {
     if (cmp == "large") {
@@ -77,4 +121,4 @@ class Heap {
 }
 
 
-module.exports = Heap;
+nthUglyNumber(10)
